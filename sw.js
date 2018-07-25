3d2d91dc25bebb974749a9bcfbe1f6a91d73c134
Sonia Gorla <soniagorla@msn.com>
2018-07-24 21:03:54 -0500

    Add map and service worker

---
+++ sw.js
@@ -0,0 +1,50 @@
+let staticCacheName = 'restaurant-static-v1';
+let urlsToCache = [
+  '/',
+  'index.html',
+  'restaurant.html',
+  'css/styles.css',
+  'data/restaurants.json',
+  'sw_registration.js',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => cache.addAll(urlsToCache))
+    .catch(error => console.log(error))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+    .then(cacheNames => Promise.all(
+        cacheNames.filter(cacheName => cacheName.startsWith('restaurant-') &&
+        cacheName != staticCacheName)
+        .map(cacheName => caches.delete(cacheName))
+      ))
+    .catch(error => console.log(error))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  let cacheRequest = event.request;
+  event.respondWith(
+    caches.match(cacheRequest)
+    .then(response => response || fetch(cacheRequest))
+  );
+});
