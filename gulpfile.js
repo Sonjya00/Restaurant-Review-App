620a0ace111a30d0693108b06b87d330f2b73738
Sonia Gorla <soniagorla@msn.com>
2018-08-05 02:06:20 -0500

    Minor fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,8 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 const sass = require('gulp-sass');
-//const eslint = require('gulp-eslint');
 
-// //Fix js files
-// gulp.task('eslint', () => {
-//   return gulp.src(['**/*.js','!node_modules/**'])
-//   .pipe(eslint())
-//   .pipe(eslint.format())
-//   .pipe(eslint.failAfterError());
-// })
-
+// BrowserSync
 gulp.task('serve', ['sass'], () => {
 	browserSync.init({
 		injectChanges: true,
@@ -34,12 +26,5 @@ gulp.task('sass', () => {
 		.pipe(gulp.dest('app/css'))
 		.pipe(browserSync.stream({match: 'app/css/*.css'}));
 });
-
-// gulp.task('watch', () =>{
-// 	gulp.watch('app/*.html').on('change', reload);
-// 	gulp.watch('app/scss/*.scss', ['sass']).on('change', reload);
-// 	gulp.watch('app/js/*.js', reload);
-// 	gulp.watch('app/*.js', reload);
-// });
 
 gulp.task('default', ['serve']);
