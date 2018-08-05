e940671a0051818d3cfbd94e02f436286465efec
Sonia Gorla <soniagorla@msn.com>
2018-08-05 00:50:21 -0500

    Fix service worker, add missing url to cache

--- app/sw.js
+++ app/sw.js
@@ -3,11 +3,11 @@ let urlsToCache = [
 	'/',
 	'index.html',
 	'restaurant.html',
-	//toremove
 	'config.js',
 	'leaflet/leaflet.css',
 	'leaflet/leaflet.js',
 	'css/styles.css',
+	'css/responsive.css',
 	'data/restaurants.json',
 	'sw_registration.js',
 	'js/dbhelper.js',
@@ -23,16 +23,19 @@ let urlsToCache = [
 	'img/8.jpg',
 	'img/9.jpg',
 	'img/10.jpg',
-	'img/cover/1.jpg',
-	'img/cover/2.jpg',
-	'img/cover/3.jpg',
-	'img/cover/4.jpg',
-	'img/cover/5.jpg',
-	'img/cover/6.jpg',
-	'img/cover/7.jpg',
-	'img/cover/8.jpg',
-	'img/cover/9.jpg',
-	'img/cover/10.jpg',
+	'img/cover_img/1.jpg',
+	'img/cover_img/2.jpg',
+	'img/cover_img/3.jpg',
+	'img/cover_img/4.jpg',
+	'img/cover_img/5.jpg',
+	'img/cover_img/6.jpg',
+	'img/cover_img/7.jpg',
+	'img/cover_img/8.jpg',
+	'img/cover_img/9.jpg',
+	'img/cover_img/10.jpg',
+	'https://fonts.gstatic.com/s/raleway/v12/1Ptug8zYS_SKggPNyCMIT4ttDfCmxA.woff2',
+	'https://fonts.gstatic.com/s/raleway/v12/1Ptug8zYS_SKggPNyC0IT4ttDfA.woff2',
+	'https://fonts.googleapis.com/css?family=Raleway',
 ];
 
 self.addEventListener('install', event => {
@@ -47,7 +50,7 @@ self.addEventListener('activate', event => {
 	event.waitUntil(
 		caches.keys()
 			.then(cacheNames => Promise.all(
-				cacheNames.filter(cacheName => cacheName.startsWith('restaurant-') &&
+				cacheNames.filter(cacheName => cacheName.startsWith('restaurant-static') &&
         cacheName != staticCacheName)
 					.map(cacheName => caches.delete(cacheName))
 			))
@@ -57,9 +60,6 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
 	let cacheRequest = event.request;
-	// if (cacheRequest.url.indexOf('restaurant.html') !== -1) {
-	//   cacheRequest = new Request('restaurant.html')
-	// }
 	event.respondWith(
 		caches.match(cacheRequest)
 			.then(response => response || fetch(cacheRequest))
--- app/sw_registration.js
+++ app/sw_registration.js
@@ -1,6 +1,6 @@
 if (navigator.serviceWorker) {
 	navigator.serviceWorker
 		.register('sw.js')
-		.then(registration => console.log('SW registration successful: ' + registration.scope))
-		.catch(error => console.log('SW registration failed: ' + error));
+		.then(registration => console.log('SW Registered: ' + registration.scope))
+		.catch(error => console.log('SW Registration Failed: ' + error));
 }
